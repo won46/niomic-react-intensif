79ecb4b0b0ad189ff1a9a779e414eba440498f87
won46 <wonn46@gmail.com>
2022-05-10 08:11:21 +0700

    tambah header

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Halo Iwan react
+          Halo Iwan react 2
         </a> 
       </header>
     </div>
